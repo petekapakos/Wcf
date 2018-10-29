ca51947a9933791572afcdf4521a91d306cbcb1a
petekapakos <pkapakos@gmail.com>
2018-10-29 15:21:20 -0700

    Add Project

---
+++ InternalService/IInternalService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using InternalService.Services.External;
+
+namespace InternalService
+{
+    [ServiceContract]
+    public interface IInternalService
+    {
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+    
+}
---
+++ InternalService/InternalService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using InternalService.Services.External;
+
+namespace InternalService
+{
+    public class InternalService : IInternalService
+    {
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            using (var service = new Service1Client())
+                return service.GetDataUsingDataContract(composite);
+        }
+    }
+}
